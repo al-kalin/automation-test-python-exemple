980496ebaedc4cb52735b8690351b45ae2a5b1a6
alkalin <alkalinko@gmail.com>
2024-01-16 23:57:53 +0200

    add assert mesage

--- conftest.py
+++ conftest.py
@@ -3,7 +3,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
-
 @pytest.fixture(scope='function')
 def driver():
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
--- tests/elements_test.py
+++ tests/elements_test.py
@@ -3,6 +3,7 @@ import time
 from pages.elements_page import TextBoxPage
 
 
+
 class TestElements:
     class TestTextBox:
 
@@ -11,7 +12,7 @@ class TestElements:
             text_box_page.open()
             full_name, email, current_address, permanent_address = text_box_page.fill_all_fields()
             output_full_name, output_email, output_cur_addr, output_per_addr = text_box_page.check_filled_form()
-            assert full_name == output_full_name
-            assert email == output_email
-            assert current_address == output_cur_addr
-            assert permanent_address == output_per_addr
+            assert full_name == output_full_name, "The full name does not match"
+            assert email == output_email, "The email does not match"
+            assert current_address == output_cur_addr, "The current address does not match"
+            assert permanent_address == output_per_addr, "The permanent address does not match"
