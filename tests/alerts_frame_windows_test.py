486d6aac4599d9286bf276681750fb6520049003
alkalin <alkalinko@gmail.com>
2024-02-11 12:45:02 +0200

    update TestAlertsFrameWindows, added class :TestAlertsPage, added : test_see_alert, test_alert_appear_5_sec, test_confirm_alert, test_prompt_alert

--- tests/alerts_frame_windows_test.py
+++ tests/alerts_frame_windows_test.py
@@ -1,4 +1,4 @@
-from pages.alerts_frame_windows_page import BrowserWindowsPage
+from pages.alerts_frame_windows_page import BrowserWindowsPage, AlertsPage
 
 
 class TestAlertsFrameWindows:
@@ -16,3 +16,31 @@ class TestAlertsFrameWindows:
             browser_window_page.open()
             text_result = browser_window_page.check_opened_new_tab()
             assert text_result == 'This is a sample page', 'A new tab window not opened or incorrect window has opened'
+
+    class TestAlertsPage:
+
+        def test_see_alert(self, driver):
+            alerts_page = AlertsPage(driver, "https://demoqa.com/alerts")
+            alerts_page.open()
+            alert_text = alerts_page.check_see_alert()
+            assert alert_text == 'You clicked a button', "Alert did not show up"
+
+        def test_alert_appear_5_sec(self, driver):
+            alerts_page = AlertsPage(driver, "https://demoqa.com/alerts")
+            alerts_page.open()
+            alert_text = alerts_page.check_alert_appear_5_sec()
+            assert alert_text == 'This alert appeared after 5 seconds', "Alert did not show up"
+
+        def test_confirm_alert(self, driver):
+            alerts_page = AlertsPage(driver, "https://demoqa.com/alerts")
+            alerts_page.open()
+            alert_text = alerts_page.check_confirm_alert()
+            assert alert_text == 'You selected Ok', "Alert did not show up"
+
+        def test_prompt_alert(self, driver):
+            alerts_page = AlertsPage(driver, "https://demoqa.com/alerts")
+            alerts_page.open()
+            text, alert_text = alerts_page.check_prompt_alert()
+            print(text)
+            print(alert_text)
+            assert alert_text == 'You entered ' + text, "Alert did not show up"
